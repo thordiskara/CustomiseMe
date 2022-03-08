036fd98f0f4dee9dcd5f35cff67cfcb1211bfc87
Nancy <nanc0603@stud.kea.dk>
2022-03-08 10:43:59 +0100

    productlist structure

--- productlist.js
+++ productlist.js
@@ -2,12 +2,34 @@
 // the API key: 622709f1dced170e8c83a044
 
 const url = "https://customiseme-2aec.restdb.io/rest/customiseme";
-
-fetch(url)
+const options = {
+  headers: {
+    "x-apikey": "622709f1dced170e8c83a044",
+  },
+};
+fetch(url, options)
   .then(function (res) {
     return res.json();
   })
   .then(function (data) {
-    showProductlist(data);
+    showSoaplist(data);
   });
-function showProductlist(data) {}
+function showSoaplist(data) {
+  console.log(data);
+  data.forEach(showSoap);
+}
+function showSoap(soap) {
+  // grabbing the tamplate
+  const template = document.querySelector("#productlistTemplate").content;
+  // copying the template
+  const copy = template.cloneNode(true);
+  copy.querySelector("img").src = `${soap.images}`;
+  copy.querySelector("h3").textContent = `${soap.name}`;
+  copy.querySelector("span").textContent = `${soap.price}`;
+  copy.querySelector("a").setAttribute("href", `product.html?id=${soap._id}`);
+
+  // grabing parent
+  const parent = document.querySelector("main");
+  // append
+  parent.appendChild(copy);
+}
